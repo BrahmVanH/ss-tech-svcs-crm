8147dbbd90f4d88667ce78080e8d72da6ad39374
Brahm Van Houzen <brahmvanh@gmail.com>
2024-05-28 00:48:56 -0400

    changed theme colors. added .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { lazy, Suspense } from 'react';
 import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
 import Navbar from './components/Navbar';
 import Loading from './components/LoadingAnimation';
-
-const Dashboard = lazy(() => import('./pages/Dashboard'));
-const EditPhotos = lazy(() => import('./pages/EditPhotos'));
+import EditPhotos from './pages/EditPhotos';
+import Dashboard from './pages/Dashboard';
+// const Dashboard = lazy(() => import('./pages/Dashboard'));
+// const EditPhotos = lazy(() => import('./pages/EditPhotos'));
 
 // const cache = new InMemoryCache({
 // 	typePolicies: {
@@ -52,9 +52,9 @@ const client = new ApolloClient({
 
 function App() {
 	const theme = {
-		primary: '#5f8fa5',
-		primaryStroke: ['#ffffff', '#abccd8', '#5f8fa5', '#abccd8', '#ffffff'],
-		secondary: 'rgb(200, 188, 167, 0.6)',
+		primary: '#57779d',
+		primaryStroke: '#fff',
+		secondary: '#c7a96e',
 	};
 
 	return (
@@ -63,8 +63,8 @@ function App() {
 				<ThemeProvider theme={theme}>
 					<Navbar />
 					<Routes>
-						<Route path='/' element={<DashboardPage />} />
-						<Route path='/photos/:property' element={<EditPhotosPage />} />
+						<Route path='/' element={<Dashboard />} />
+						<Route path='/photos/:property' element={<EditPhotos />} />
 					</Routes>
 				</ThemeProvider>
 			</ApolloProvider>
@@ -74,14 +74,5 @@ function App() {
 
 export default App;
 
-const DashboardPage = () => (
-	<Suspense fallback={<Loading />}>
-		<Dashboard />
-	</Suspense>
-);
 
-const EditPhotosPage = () => (
-	<Suspense fallback={<Loading />}>
-		<EditPhotos />
-	</Suspense>
-);
+
